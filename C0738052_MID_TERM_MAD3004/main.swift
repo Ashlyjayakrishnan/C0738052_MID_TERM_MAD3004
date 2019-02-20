55852ea5cccd0a64bba86970f2049502305acef2
Ashlyjayakrishnan <ashlybaby95@gmail.com>
2019-02-20 18:25:32 -0500

    Initial Commit

---
+++ C0738052_MID_TERM_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0738052_MID_TERM_MAD3004
+//
+//  Created by Ashly Baby on 2019-02-20.
+//  Copyright © 2019 Ashly Baby. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
